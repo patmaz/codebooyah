d8309a676a05577a4571ff783948c9ef1f26c444
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-02-08 11:31:27 +0100

    first commit one route

---
+++ index.js
@@ -0,0 +1,19 @@
+// ############### web server by express
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+
+//modules
+var routes = require("./modules/routes");
+
+//static files
+app.use('/static', express.static(__dirname + "/public"));
+
+// EJS engine
+app.set("view engine", "ejs");
+
+//controllers
+routes.index(app);
+
+//environmental variables on server or 3000
+http.listen(3000);
---
+++ modules/routes.js
@@ -0,0 +1,9 @@
+function index(app) {
+    app.get("/", function(req, res){
+        res.render("index");
+    });
+}
+
+module.exports = {
+    index: index
+}
