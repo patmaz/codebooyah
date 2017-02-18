a7cb2b3d2627ca290b20371752c55a3bced58ca4
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-02-18 18:28:53 +0100

    intro modification

--- src/js/components/Intro.jsx
+++ src/js/components/Intro.jsx
@@ -3,13 +3,45 @@ import React from 'react';
 class Intro extends React.Component {
    render() {
         return (
-            <div id="intro">
+            <div className={'intro'}>
                 <p>Welcome to my JavaScript sandbox. Have fun with:</p>
                 <ul>
-                    <li><a href='#/gif'>react gif search</a><br /></li>
-                    <li><a href='#/chat'>simple node.js/react chat</a></li>
-                    <li><a href='#/github'>my github repositories search on react</a></li>
-                    <li><a href='#/stopwatch'>react stopwatch</a></li>
+                    <li className={'intro__link'}>
+                        <a className={'brackets'} href='#/gif'>gif search</a>
+                        <div className={'intro__tags'}>
+                            <span>reactjs</span>
+                            <span>jsx</span>
+                            <span>promise</span>
+                            <span>ajax</span>
+                        </div>
+                    </li>
+                    <li className={'intro__link'}>
+                        <a className={'brackets'} href='#/chat'>simple chat</a>
+                        <div className={'intro__tags'}>
+                            <span>reactjs</span>
+                            <span>jsx</span>
+                            <span>nodejs</span>
+                            <span>websocket</span>
+                        </div>
+                    </li>
+                    <li className={'intro__link'}>
+                        <a className={'brackets'} href='#/github'>my github repositories search engine</a>
+                        <div className={'intro__tags'}>
+                            <span>reactjs</span>
+                            <span>jsx</span>
+                            <span>promise</span>
+                            <span>ajax</span>
+                        </div>
+                    </li>
+                    <li className={'intro__link'}>
+                        <a className={'brackets'} href='#/stopwatch'>react stopwatch</a>
+                        <div className={'intro__tags'}>
+                            <span>ES6</span>
+                            <span>reactjs</span>
+                            <span>jsx</span>
+                            <span>localStorage</span>
+                        </div>
+                    </li>
                 </ul>
             </div>
         )
