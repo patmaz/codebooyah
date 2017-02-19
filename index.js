650c05b6d3f1c8e5fdef46f4a3ff4bbb74aa3334
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-02-19 18:50:48 +0100

    webrtc first wroking version

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 var express = require("express");
 var app = express();
 var http = require('http').Server(app);
+// var https = require('https');
+// var fs = require('fs');
+const websocket = require('ws');
+const WebSocketServer = websocket.Server;
 
 //modules
 var mongo = require('./modules/mongo');
@@ -22,3 +26,32 @@ chat(http);
 
 //environmental variables on server or 3000
 http.listen(8000);
+
+// var options = {
+//   key: fs.readFileSync('./ssl/key.pem'),
+//   cert: fs.readFileSync('./ssl/cert.pem')
+// };
+
+// var sslSrv = https.createServer(options, app).listen(8000);
+
+// var wss = new WebSocketServer({server: http});
+
+// // Broadcast to all.
+// wss.broadcast = function broadcast(data) {
+//   wss.clients.forEach(function each(client) {
+//     if (client.readyState === websocket.OPEN) {
+//       client.send(data);
+//     }
+//   });
+// };
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(data) {
+//     // Broadcast to everyone else.
+//     wss.clients.forEach(function each(client) {
+//       if (client !== ws && client.readyState === websocket.OPEN) {
+//         client.send(data);
+//       }
+//     });
+//   });
+// });
