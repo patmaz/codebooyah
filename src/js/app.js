a0ada069a36bdd7e7649b515cd890a13ee612ba1
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-02-25 16:39:00 +0100

    HMR only reload part 2

--- src/js/app.js
+++ src/js/app.js
@@ -9,14 +9,26 @@ import GithubSearch from './components/GithubSearch.jsx';
 import Stopwatch from './components/Stopwatch.jsx';
 import ToDo from './comp-todo/containers/App.jsx';
 
+class Root extends React.Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path='/' component={() => (<Intro />)} />
+                <Route path='/list' component={() => (<ListItems />)} />
+                <Route path='/gif' component={() => (<SearchGifApp />)} />
+                <Route path='/chat' component={() => (<Chat />)} />
+                <Route path='/github' component={() => (<GithubSearch />)} />
+                <Route path='/stopwatch' component={() => (<Stopwatch />)} />
+                <Route path='/todo' component={() => (<ToDo />)} />
+            </Router>
+        )
+    }
+}
+
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path='/' component={() => (<Intro />)} />
-        <Route path='/list' component={() => (<ListItems />)} />
-        <Route path='/gif' component={() => (<SearchGifApp />)} />
-        <Route path='/chat' component={() => (<Chat />)} />
-        <Route path='/github' component={() => (<GithubSearch />)} />
-        <Route path='/stopwatch' component={() => (<Stopwatch />)} />
-        <Route path='/todo' component={() => (<ToDo />)} />
-    </Router>,
+    <Root />,
     document.getElementById('app'));
+
+if (module.hot) {
+    module.hot.accept()
+}
