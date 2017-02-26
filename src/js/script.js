24178fc2e496b8acccc0560d98b393e5dd16c541
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-02-26 09:05:38 +0100

    svg anim background improved

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,50 @@
 (function(win, doc) {
     function start() {
-        var about = $('.about');
-        about.on('click', function(e) {
+        var $about = $('.about');
+        $about.on('click', function(e) {
             if ($(e.target).hasClass('about')) {
                 $(this).toggleClass('show');
             }
         });
-        if(anime !== 'undefined') {
-        	var brackets = anime({
-	            targets: '#svg-bgr path',
-	            strokeDashoffset: [anime.setDashoffset, 0],
-	            easing: 'easeInOutSine',
-	            duration: 2000,
-	            delay: function(el, i) { return (i * 200) },
-	            direction: 'alternate',
-	            autoplay: true,
-	        });
 
-	        var booyah = anime({
-	            targets: '#svg-bgr text',
-	            translateY: 10,
-	            direction: 'alternate',
-	            delay: 150,
-	            duration: function(el, i, l) {
-	                return (i * 400);
-	            },
-	            complete: function(anim) {
-	                var booyahbye = anime({
-					  targets: '#svg-bgr text',
-					  opacity: 0,
-					  delay: function(el, i) { return (i * 100) },
-					});
-	            }
-	        });
+        if(anime !== 'undefined' && window.innerWidth > 767) {
+            var appDiv = anime({
+                targets: '#app',
+                scale: 0,
+                rotate: '1turn',
+                direction: 'reverse',
+                delay: 0,
+                duration: 5000
+            });
+
+            var brackets = anime({
+                targets: '#svg-bgr path',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeInOutSine',
+                duration: 2000,
+                delay: function(el, i) { return (i * 200) },
+                direction: 'alternate',
+                autoplay: true,
+            });
+
+            var booyah = anime({
+                targets: '#svg-bgr text',
+                translateX: 10,
+                direction: 'alternate',
+                delay: 300,
+                duration: function(el, i, l) {
+                    return ((i+1) * 300);
+                },
+                complete: function(anim) {
+                    var booyahbye = anime({
+                      targets: '#svg-bgr text',
+                      opacity: 0,
+                      delay: function(el, i) { return (i * 100) },
+                    });
+                }
+            });
         } else {
-        	document.getElementById('svg-bgr').style.display = 'none';
+            document.getElementById('svg-bgr').style.display = 'none';
         }
     }
     $(doc).ready(start);
