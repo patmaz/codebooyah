43f62a598b3bd910e8a50ce4005ef7dd1288a4be
Patryk Mazurkiewicz <patrykiewicz@gmail.com>
2019-01-07 17:56:17 +0100

    Aboust update

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -21,13 +21,9 @@ export class About extends React.Component {
     return (
       <div onClick={this.toggleShow} className={isVisible ? "about code show" : "about code"}>
         <div className="about__inner">
-          <span>{'< web dev/>'}</span>
           <span className="brackets"><a href="https://github.com/patmaz" target="_blank" rel="noopener noreferrer">github</a></span>
+          <span className="brackets"><a href="https://codesandbox.io/u/patmaz" target="_blank" rel="noopener noreferrer">codesandbox</a></span>
           <span className="brackets"><a href="https://www.linkedin.com/in/patmazurkiewicz" target="_blank" rel="noopener noreferrer">linkedin</a></span>
-          <span>{'< hobbies/>'}</span>
-          <span className="brackets"><a href="http://yt.codebooyah.com/" target="_blank" rel="noopener noreferrer">youtube</a></span>
-          <span className="brackets"><a href="http://insta.codebooyah.com/" target="_blank" rel="noopener noreferrer">instagram</a></span>
-          <span className="brackets"><a href="http://beer.codebooyah.com/" target="_blank" rel="noopener noreferrer">craft beer</a></span>
         </div>
       </div>
     )
